ac371f19f281a44ffaebe3a44f2050be462d8528
Maritza Sinti <maritzasintitafur@gmail.com>
2021-08-06 19:04:13 -0500

    Add base nuxt structure

---
+++ nuxt.config.js
@@ -0,0 +1,53 @@
+export default {
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	ssr: true,
+	head: {
+		title: "pokedex",
+		htmlAttrs: {
+			lang: "en"
+		},
+		meta: [
+			{ charset: "utf-8" },
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1"
+			},
+			{ hid: "description", name: "description", content: "" },
+			{ name: "format-detection", content: "telephone=no" }
+		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+	},
+
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
+
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
+
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
+
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [],
+
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: ["@nuxtjs/axios"],
+	axios: {
+		proxy: true
+	},
+	proxy: {
+		"/api/pokemon": {
+			target: process.env.API_POKEHABITAD_SERVICE,
+			pathRewrite: { "^/api/pokemon": "/" }
+		}
+	},
+
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {
+		babel: {
+			plugins: [
+				["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+			]
+		}
+	}
+};
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+	div Hello Wordl! 
+</template>
+
+<script>
+export default {};
+</script>
