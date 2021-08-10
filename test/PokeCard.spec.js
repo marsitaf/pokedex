3759f74b4b79043650684a1f0692effc5826108b
Maritza <maritza.sinti@arisale.com.pe>
2021-08-10 13:20:38 -0500

    Remove unnecesary await

--- test/PokeCard.spec.js
+++ test/PokeCard.spec.js
@@ -4,8 +4,7 @@ import { shallowMount } from "@vue/test-utils"
 describe("Card", () => {
     it("Emits a select event on click", async () => {
         const wrapper = shallowMount(Card)
-        await wrapper.trigger("click").then(() => {
-            expect(wrapper.emitted().select[0]).toEqual([])
-        })
+        wrapper.trigger("click")
+        expect(wrapper.emitted().select[0]).toEqual([])
     })
 })
